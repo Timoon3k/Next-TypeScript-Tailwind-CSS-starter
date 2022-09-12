9d0b3860babe60304b700f8bdba701cf318f87db
Tomasz <tomasz.majewski1994@gmail.com>
2022-09-13 01:21:57 +0200

    configure start pack

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react'
-import Layout from '../components/Layout'
 import { NextPage } from 'next'
 
 const IndexPage: NextPage = () => {
 	return (
-		<Layout title='Home | Next.js + TypeScript Starter'>
-			<h1 className='h-20 underline'>Hello Next.js 👋</h1>
-		</Layout>
+		<p className='font-mono text-lg underline decoration-wavy shadow-sm translate-y-3 '>
+			Next.js | TypeScript | TailwindCSS - starter pack
+		</p>
 	)
 }
 
